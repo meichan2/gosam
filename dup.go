515f7e26717785773c1bd08d162f1b6592dcca57
mei <76828746+meichan2@users.noreply.github.com>
2021-01-25 22:15:24 +0900

    Create dup.go

---
+++ dup.go
@@ -0,0 +1,23 @@
+package main
+//2回以上現れる行を出現回数とともに表示する。
+
+import(
+  "bufio"
+  "fmt"
+  "os"
+
+)
+
+func main() {
+  counts:=make(map[string]int)
+  input:=bufio.NewScanner(os.Stdin)
+  for input.Scan(){
+    counts[input.Text()]++
+  }
+  for line,n := range counts {
+
+    if n>1{
+      fmt.Printf("%d\t%s\n",n,line)
+    }
+  }
+}
